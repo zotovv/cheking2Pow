dfa414f5e34cdb3beec5090300214c871d6a1248
Vladimir Zotov <zott14@outlook.com>
2018-03-22 08:09:44 +0300

    Add project files.

---
+++ cheking2Pow/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cheking2Pow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
